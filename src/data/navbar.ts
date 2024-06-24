70877e8739720a5eeec8d6d6fdfd1ad3b392467c
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-06-23 23:45:11 -0400

    Refactor: Create file data from navbar items and import in Header. Fix description text from AboutMe

---
+++ src/data/navbar.ts
@@ -0,0 +1,28 @@
+const NAVBAR = [
+  {
+    title: "Experiencia Laboral",
+    label: "experiencia",
+    url: "/#experiencia-laboral",
+    target: "_self"
+  },
+  {
+    title: "Proyectos",
+    label: "proyectos",
+    url: "/#proyectos",
+    target: "_self"
+  },
+  {
+    title: "Sobre mí",
+    label: "sobre-mi",
+    url: "/#sobre-mi",
+    target: "_self"
+  },
+  {
+    title: "Contacto",
+    label: "contacto",
+    url: "mailto:rafalejandrorivero@gmail.com?subject=Contacto&body=Hola, Rafael!",
+    target: "_blank"
+  }
+]
+
+export default NAVBAR;
