f5cf3e39a95f74658abde472e296b314988179f2
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-06-24 17:08:08 -0400

    Feature: Create file data from aboutMe by store description and image

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,12 @@
+const ABOUTME = {
+    description: `Me llamo Rafael Rivero. Cuento con experiencia en el Desarrollo de Aplicaciones Web y API Rest, principalmente en la automatización de procesos. 
+    Actualmente soy 
+    <strong style="color:rgb(134 239 172 / 1);font-weight:400;font-family:ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, monospace;"> 
+        Desarrollador Full-Stack 
+    </strong> 
+    para una Startup importante del sector Telecomunicaciones.`,
+    image: "/images/profilePhoto.png",
+    imageAlt: "Rafael Rivero"
+};
+
+export default ABOUTME;
