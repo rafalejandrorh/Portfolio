fd046d0dc37f24cdf7f9fad2a21d848e9a105fd9
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-08-02 20:42:09 -0400

    Feature: Implementing Environment Variables

--- src/data/aboutMe.ts
+++ src/data/aboutMe.ts
@@ -1,12 +1,14 @@
+import environments from "../data/environments";
+
 const ABOUTME = {
-    description: `Me llamo Rafael Rivero. Cuento con experiencia en el Desarrollo de Aplicaciones Web y API Rest, principalmente en la automatización de procesos. 
+    description: `Me llamo ${environments.fullNameDeveloper}. Cuento con experiencia en el Desarrollo de Aplicaciones Web y API Rest, principalmente en la automatización de procesos. 
     Actualmente soy 
     <strong style="color:rgb(134 239 172 / 1);font-weight:400;font-family:ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, monospace;"> 
         Desarrollador Full-Stack 
     </strong> 
     para una Startup importante del sector Telecomunicaciones.`,
     image: "/images/profilePhoto.png",
-    imageAlt: "Rafael Rivero"
+    imageAlt: environments.fullNameDeveloper
 };
 
 export default ABOUTME;
---
+++ src/data/environments.ts
@@ -0,0 +1,13 @@
+const environments = {
+    applicationVersion: import.meta.env.APPLICATION_VERSION,
+    fullNameDeveloper: import.meta.env.FULL_NAME_DEVELOPER,
+    nameDeveloper: import.meta.env.NAME_DEVELOPER,
+    userLinkedin: import.meta.env.USER_LINKEDIN,
+    userGithub: import.meta.env.USER_GITHUB,
+    userTelegram: import.meta.env.USER_TELEGRAM,
+    phoneNumber: import.meta.env.PHONE_NUMBER,
+    email: import.meta.env.EMAIL,
+    githubRepository: import.meta.env.GITHUB_REPOSITORY
+};
+
+export default environments;
